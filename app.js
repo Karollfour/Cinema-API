95f821c5163d97d681efc9e7d8ad5a45579da517
Karolline Fernandes <145523605+Karollfour@users.noreply.github.com>
2024-06-17 22:25:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Importar Rotas
-const movieRoutes = require('./src/routes/movieRoutes'); // Certifique-se de que o caminho está correto
-const showtimeRoutes = require('./src/routes/showtimeRoutes'); // Certifique-se de que o caminho está correto
+const movieRoutes = require('./src/routes/movieRoutes'); 
+const showtimeRoutes = require('./src/routes/showtimeRoutes'); 
 
 app.use(express.json());
-// Usar Rotas
+
 app.use('/api/movies', movieRoutes);
 app.use('/api/showtimes', showtimeRoutes);
 
